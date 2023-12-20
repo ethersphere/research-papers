c87ba60b901113412880b0e43541ac45c528caf6
Martin <martin.koterle@gmail.com>
2023-12-20 13:57:28 +0100

    First

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import relativeLinks from "astro-relative-links";
+
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  trailingSlash: 'always',
+  integrations: [tailwind(), relativeLinks(), alpinejs()]
+});
